7fdc10bf6a955df49eeb519a847b2b8a7f5cc105
Cesar Reneses <c.reneses.carcamo@avanade.com>
2026-03-19 09:45:07 +0100

    Add project files.

---
+++ Helpdesk.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helpdesk.Infrastructure.Identity
+{
+    internal class ApplicationUser
+    {
+    }
+}
---
+++ Helpdesk.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helpdesk.Infrastructure.Persistence
+{
+    internal class AppDbContext
+    {
+    }
+}
