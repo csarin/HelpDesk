f7042cbba1bd776a61ea9c897784c5afe21bd250
Cesar Reneses <c.reneses.carcamo@avanade.com>
2026-03-20 11:10:28 +0100

    Add migrations, clean up references, and start using LocalDB for local testing

--- Helpdesk.Infrastructure/Persistence/DbInitializer.cs
+++ Helpdesk.Infrastructure/Persistence/DbInitializer.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Helpdesk.Domain.Entities;
+﻿using Helpdesk.Domain.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
