2720f78671f482a95c27ee24be6694ef1ded6579
Cesar Reneses <c.reneses.carcamo@avanade.com>
2026-03-24 12:14:36 +0100

    Added create page styles screen and mockup data

--- Helpdesk.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
+++ Helpdesk.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,8 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Helpdesk.Application.Services;
-using Microsoft.EntityFrameworkCore;
-using Helpdesk.Infrastructure.Persistence;
 
 namespace Helpdesk.Infrastructure.DependencyInjection
 {
@@ -10,11 +8,7 @@ namespace Helpdesk.Infrastructure.DependencyInjection
     {
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
-            if (string.IsNullOrWhiteSpace(connectionString))
-                throw new InvalidOperationException("Connection string does not exist 'DefaultConnection'.");
-
-            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+            _ = configuration;
 
             services.AddScoped<ITicketService, Helpdesk.Infrastructure.Services.TicketService>();
             return services;
